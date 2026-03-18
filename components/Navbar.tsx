0bcfd393347322df7967394066136e46fbbc1599
Diego Coronado <diegocoronado8@hotmail.com>
2026-03-18 15:03:38 +0900

    add navbaar

---
+++ components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { Box } from "lucide-react";
+
+import Button from "./ui/Button";
+
+const isSignedId = false;
+const username = "test";
+
+const Navbar = () => {
+  const handleAuthClick = async () => {};
+
+  return (
+    <header className="navbar">
+      <nav className="inner">
+        <div className="left">
+          <div className="brand">
+            <Box className="logo" />
+            <span className="name">Roomify</span>
+          </div>
+
+          <ul className="links">
+            <a href="#">Products</a>
+            <a href="#">Pricing</a>
+            <a href="#">Community</a>
+            <a href="#">Enterprise</a>
+          </ul>
+        </div>
+
+        <div className="actions">
+          {isSignedId ? (
+            <>
+              <span className="greeting">
+                {username ? `Hi, ${username}` : "Signed in"}
+              </span>
+
+              <Button size="sm" onClick={handleAuthClick} className="btn">
+                Log Out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button onClick={handleAuthClick} size="sm" variant="ghost">
+                Log In
+              </Button>
+              <a href="#upload" className="cta">
+                Get Started
+              </a>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
